3f1cef6583352aacef19552151ce2eab7aa39709
Drake0112 <praneethreddy0112@gmail.com>
2026-03-12 20:41:02 +0530

    fixed some errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,11 @@ import app from "./app";
 
 const PORT = process.env.PORT || 5000;
 
+// health check route
+app.get("/", (req, res) => {
+  res.send("AI Smart Travel Planner Backend Running 🚀");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
