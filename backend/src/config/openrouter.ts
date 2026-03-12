4a9b87965b76ce0e250b3263fe778d3adc0b7695
Drake0112 <praneethreddy0112@gmail.com>
2026-03-12 20:56:02 +0530

    added openruter

--- backend/src/config/openrouter.ts
+++ backend/src/config/openrouter.ts
@@ -3,6 +3,10 @@ import OpenAI from "openai";
 const client = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1",
   apiKey: process.env.OPENROUTER_API_KEY,
+  defaultHeaders: {
+    "HTTP-Referer": "https://aismarttravelplanner-production.up.railway.app",
+    "X-Title": "AI Smart Travel Planner"
+  }
 });
 
 export default client;
